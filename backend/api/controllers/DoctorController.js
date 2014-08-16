1ca7efdbe66ab34e4105e853b602c49e3f8ce429
Paul Vorobyev <paul.vorobyev@gmail.com>
2014-08-16 11:56:13 -0400

    config + generate APIs

---
+++ backend/api/controllers/DoctorController.js
@@ -0,0 +1,11 @@
+/**
+ * DoctorController
+ *
+ * @description :: Server-side logic for managing doctors
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ backend/api/controllers/PatientController.js
@@ -0,0 +1,11 @@
+/**
+ * PatientController
+ *
+ * @description :: Server-side logic for managing patients
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
