51ff185c567ac2359fa560710cc076d572eda2b7
Paul Vorobyev <paul.vorobyev@gmail.com>
2014-08-16 13:54:39 -0400

    Switch to local MongoDB

--- backend/api/models/Medication.js
+++ backend/api/models/Medication.js
@@ -25,7 +25,7 @@ module.exports = {
     uniqueCode: {
       type: "STRING",
       required: true
-    }
+    },
 
     supposedToTakeToday: {
       type: "BOOLEAN",
--- backend/config/connections.js
+++ backend/config/connections.js
@@ -1,11 +1,13 @@
 module.exports.connections = {
 
   mongoDB: {
-    adapter: 'sails-mongo',
-    host: 'mongodb://admin:admin@kahana.mongohq.com:10032/breakBarr',
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    adapter   : 'sails-mongo',
+    host      : 'localhost',
+    port      : 27017,
+    // user      : 'admin',
+    // password  : 'admin',
+    database  : 'BBhax'
   }
-  
+
+
 };
